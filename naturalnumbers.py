4efb9a0fdb8309c1d819acc0605f675af6e185d4
Ahmedullah10 <khaanahmed1988@gmail.com>
2026-08-17 18:17:44 +0500

    Add files via upload

---
+++ naturalnumbers.py
@@ -0,0 +1,8 @@
+number = int(input("Entre number to check"))
+print("Entre numbers to be checked :", number)
+
+if number>0:
+    print("This is natural number.")
+    
+elif number<0:
+    print("This is not natural number.")
