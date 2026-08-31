243456f094224deddee11aa47292d3a45599cdb9
Ahmedullah10 <khaanahmed1988@gmail.com>
2026-08-31 19:10:47 +0500

    Add files via upload

---
+++ dictianories.py
@@ -0,0 +1,12 @@
+dic = {'name': 'Ahmedullah', 'age':12, 'grade':6}
+print(dic)
+dic['name'] = "ali"
+print(dic)
+
+dic ["age"] = 10
+print(dic)
+
+dic.pop("grade")
+print(dic)
+dic.clear()
+print(dic)
